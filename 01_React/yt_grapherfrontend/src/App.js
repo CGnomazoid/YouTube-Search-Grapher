a8f88775f13858bbd0fb087f5b7b420033855fe6
Colin Ulmer <beaus_vend0y@icloud.com>
2023-02-11 16:48:34 -0800

    Ok its a calculator now

--- 01_React/yt_grapherfrontend/src/App.js
+++ 01_React/yt_grapherfrontend/src/App.js
@@ -1,49 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import axios from 'axios';
-
-const API_KEY = 'AIzaSyBXKu5PMUUl3BIkZASPwpSUxkTncXZl9yQ';
-const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
-const MAX_RESULTS = 50;
-
-const searchPlaylistVideos = async (playlistId, searchTerm) => {
-  const userParams = {
-    part: 'snippet',
-    playlistId,
-    maxResults: MAX_RESULTS,
-    key: API_KEY,
-  };
-
-  const res = await axios.get(BASE_URL, { params: userParams });
-  const data = res.data;
-
-  let frequency = 0;
-
-  for (const item of data.items) {
-    const title = item.snippet.title;
-    const videoId = item.snippet.resourceId.videoId;
-    const videoUrl = `https://youtube.com/watch?v=${videoId}`;
-    if (title.toLowerCase().includes(searchTerm.toLowerCase())) {
-      frequency += 1;
-    }
-  }
-
-  return frequency;
-};
 
 function MyComponent() {
-  const [frequency, setFrequency] = useState(0);
+  const [value, setValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
 
-  const handleClick = async () => {
-    const result = await searchPlaylistVideos('PLoSWVnSA9vG9qV0CVCpg5WwEy3LiP7udY', 'hunted');
-    setFrequency(result);
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    setDisplayValue(value);
   };
 
   return (
     <div>
-      <button onClick={handleClick}>PRESS ME</button>
-      <p>Frequency: {frequency}</p>
+      <input type="text" value={value} onChange={handleChange} />
+      <button onClick={handleClick}>Update</button>
+      <p>{displayValue}</p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const upClick = () => {
+    setCount(count + 1);
+  };
+  const downClick = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <div className="calculatorInput">
+        <button onClick={downClick} className="calculatorButtons">-</button>
+        <button onClick={upClick} className="calculatorButtons">+</button>
+        <button onClick={() => setCount(0)} className="calculatorButtons">Reset</button>
+        <button onClick={() => setCount(count * count)} className="calculatorButtons">Squared</button>
+        <button onClick={() => setCount(count * 2)} className="calculatorButtons">Double</button>
+        <button onClick={() => setCount(count / 2)} className="calculatorButtons">Half</button>
+      </div>
     </div>
   );
 }
@@ -53,6 +52,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {MyComponent()}
+        {Counter()}
       </header>
     </div>
   );
